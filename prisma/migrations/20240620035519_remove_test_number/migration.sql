cfac4e6b06364f1e54ff242eca47e5af40b05863
hassan-kamel <60411536+hassan-kamel@users.noreply.github.com>
2024-06-20 06:57:45 +0300

    Build an API for creating issues

---
+++ prisma/migrations/20240620035519_remove_test_number/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `testNumber` on the `Issue` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Issue" DROP COLUMN "testNumber";
